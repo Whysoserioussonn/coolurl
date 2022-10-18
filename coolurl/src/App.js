cf57461f734b142d5dbfc8c70b0b16c6fe9d6850
Lucas Da Silva <lucasilva15@live.com>
2022-10-18 11:53:31 -0400

    start point

--- coolurl/src/App.js
+++ coolurl/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey guys</h1>
+      <Routes>
+        <Route path='/cats'/>
+        <Route path='/dogs'/>
+        <Route path='/zooanimals'/>
+      </Routes>
     </div>
   );
 }
