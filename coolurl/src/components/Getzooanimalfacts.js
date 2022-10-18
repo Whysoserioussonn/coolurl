0ec8a45a73a7be36db0422a6cc4bfe3abca60619
Lucas Da Silva <lucasilva15@live.com>
2022-10-18 13:46:22 -0400

    working?

--- coolurl/src/components/Getzooanimalfacts.js
+++ coolurl/src/components/Getzooanimalfacts.js
@@ -1,7 +1,27 @@
+import { useState } from "react"
+
 export default function Getzooanimalfact() {
+   
+    let [fact, setFact] = useState('')
+
+    const getFact = async () => {
+        try{
+            const response = await fetch(
+                'https://zoo-animal-api.herokuapp.com/animals/rand'
+            );
+            const data = await response.json()
+            console.log(data)
+            setFact(data)
+        }
+        catch (err){
+            console.error(err)
+        }
+    }
     return(
         <div>
-        <h1>Hi</h1>
-    </div>
+            <h1> Random Zoo animal Facts!</h1>
+            <h2> Click the button to learn about a random zoo animal!</h2>
+            <p>{fact.name}</p>
+        </div>
     )
 }
---
+++ coolurl/src/pages/Dogfacts.js
@@ -0,0 +1,5 @@
+export default function Dogfacts(){
+    return(
+        <h1>working</h1>
+    )
+}
