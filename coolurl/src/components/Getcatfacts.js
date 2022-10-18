02df56feef030f009bc7944a666b828652fe1301
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-18 14:19:09 -0400

    adding data

--- coolurl/src/components/Getcatfacts.js
+++ coolurl/src/components/Getcatfacts.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react"
+
+
+
 export default function Getcatfacts() {
-  return(
-      <div>
-      <h1>cat fact</h1>
-  </div>
-  )
+  let [fact, setFact] = useState('')
+
+    const getFact = async () => {
+        try{
+            const response = await fetch(
+                'https://api.thecatapi.com/v1/images/search'
+            );
+            const data = await response.json()
+            console.log(data)
+            setFact(data)
+        }
+        catch (err){
+            console.error(err)
+        }
+    }
+    const handleClick = () => {
+        getFact()
+    }
+    useEffect(() => {
+        getFact()
+    },[])
+    return(
+        <div>
+            <h1> Kitties!</h1>
+                  
+            <br/>
+            <img src={fact.image_link} height='500' width='500' alt='no picture available'></img>
+            <br/>
+            <p>id: {fact.id}</p>
+            <p>url: {fact.url}</p>
+            <p>width: {fact.width}</p>
+            <p>height: {fact.height}</p>
+        </div>
+    )
 }
